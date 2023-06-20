cd35e5ffac768f03ffb9e35fa2c4f35a6cf10d38
mani123 <70480001+mani1232@users.noreply.github.com>
2023-06-20 18:09:52 +0200

    Add every task

--- src/main/java/ua/mani123/Main.java
+++ src/main/java/ua/mani123/Main.java
@@ -1,12 +1,14 @@
 package ua.mani123;
 
 import mani123.ua.WebRequester;
+import mani123.ua.data.*;
 import ua.mani123.Module12.FizzBuzz;
 import ua.mani123.Module12.Task;
 import ua.mani123.module10.FileUtils;
 import ua.mani123.module11.Utils;
 import ua.mani123.module8.*;
 
+import java.io.File;
 import java.net.http.HttpResponse;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -42,10 +44,7 @@ public class Main {
         // hw 2
         list.stream().filter(s -> s instanceof String).toList().stream().map(s -> s.toString().toUpperCase()).sorted(Comparator.reverseOrder()).forEach(System.out::println);
         // hm 3
-        System.out.println(Stream.of("1, 2, 0", "3, 4, 5").flatMap(s -> Arrays.stream(s.split(", ")))
-                .map(String::trim)
-                .sorted()
-                .collect(Collectors.joining(", ")));
+        System.out.println(Stream.of("1, 2, 0", "3, 4, 5").flatMap(s -> Arrays.stream(s.split(", "))).map(String::trim).sorted().collect(Collectors.joining(", ")));
         // hw 4
         Utils.generateRandomStream(25214903917L, 11L, (long) Math.pow(2, 48)).limit(10).forEach(System.out::println);
         // hw 5
@@ -65,24 +64,35 @@ public class Main {
     }
 
     public static void module13() {
-        //HttpResponse<String> response;
-        //User user = new User();
-        //user.setId(5);
-        //user.setName("NewName");
-        //response = new WebRequester("https://jsonplaceholder.typicode.com").putUserRequest(user, "users").sendRequest();
-        //System.out.println(response.statusCode());
-        //System.out.println(response.body());
+        ArrayList<HttpResponse<String>> responses = new ArrayList<>();
+        User defaultUser = new User(11, "", "", "", new Address("", "", "", "", new Geo("", "")), "", "", new Company("", "", ""));
+        // Task 1.1
+        responses.add(new WebRequester("https://jsonplaceholder.typicode.com").postUserRequest(defaultUser, "users").sendRequest());
+        // Task 1.2
+        defaultUser.setId(9);
+        responses.add(new WebRequester("https://jsonplaceholder.typicode.com").putUserRequest(defaultUser, "users").sendRequest());
+        // Task 1.3
+        responses.add(new WebRequester("https://jsonplaceholder.typicode.com").deleteUserRequest("users", "3").sendRequest());
+        // Task 1.4
+        responses.add(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users", null, null).sendRequest());
+        // Task 1.5
+        responses.add(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users", "id", "2").sendRequest());
+        // Task 1.6
+        responses.add(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users", "username", "Leopoldo_Corkery").sendRequest());
+
+        // Check status code
+        responses.stream().filter(stringHttpResponse -> !String.valueOf(stringHttpResponse.statusCode()).startsWith("20")).toList().forEach(System.out::println);
 
         // Lol 100 files with comments
+        // Task 2
+        ArrayList<User> users = new ArrayList<>(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users", null, null).sendRequestAndReturnUserList());
+        HashMap<File, ArrayList<Comment>> data = new HashMap<>();
+        users.forEach(user ->
+                new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users/" + user.getId() + "/posts", null, null).sendRequestAndReturnPostList().forEach(post ->
+                data.put(new File(String.format("user-%s-post-%s-comments.json", user.getId(), post.getId())), new ArrayList<>(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("posts/" + post.getId() + "/comments", null, null).sendRequestAndReturnCommentList()))));
+        WebRequester.Companion.saveToFiles(data);
 
-        //ArrayList<User> users = new ArrayList<>(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users", null, null).sendRequestAndReturnUserList());
-        //HashMap<File, ArrayList<Comment>> data = new HashMap<>();
-        //users.forEach(user -> {
-        //    new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users/" + user.getId() + "/posts", null, null).sendRequestAndReturnPostList().forEach(post -> {
-        //        data.put(new File(String.format("user-%s-post-%s-comments.json", user.getId(), post.getId())), new ArrayList<>(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("posts/" + post.getId() + "/comments", null, null).sendRequestAndReturnCommentList()));
-        //    });
-        //});
-        //WebRequester.Companion.saveToFiles(data);
+        // Task 3
         System.out.println(new WebRequester("https://jsonplaceholder.typicode.com").getRequest("users/1/todos", null, null).sendRequestAndReturnTodoList().stream().filter(todo -> !todo.getCompleted()).collect(Collectors.toList()));
     }
 
